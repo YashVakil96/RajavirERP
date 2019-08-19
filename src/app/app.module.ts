669afc42109db4c0f15dbe435e6dcbfd4d732cb1
joker_yash96 <yashvakil96@gmail.com>
2019-08-19 23:15:33 +0530

    Database Test
    Non tested changes for database.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { DatabaseService } from "./database.service";
+
+import { IonicStorageModule } from "@ionic/storage";
+import { HttpModule } from "@angular/http";
+
+import { SQLitePorter } from "@ionic-native/sqlite-porter";
+import { SQLite } from "@ionic-native/sqlite";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,12 +23,17 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
+    HttpModule,
+    IonicStorageModule.forRoot(),
     AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    DatabaseService,
+    SQLite,
+    SQLitePorter
   ],
   bootstrap: [AppComponent]
 })
